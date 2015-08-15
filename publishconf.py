ed221877755f2fd567803ba7a469ae3506a6b4cb
Ellison Leão <ellisonleao@gmail.com>
2015-08-15 13:26:47 -0300

    fix publishconf / fix menu items links

--- publishconf.py
+++ publishconf.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+# Bad fix to get common confs
+import sys
+sys.path.append('.')
+
 from pelicanconf import *
 
-SITEURL = 'http://pyladies-curitiba.github.io/br-pyladies-pelican/'
+SITEURL = 'http://pyladies-curitiba.github.io/br-pyladies-pelican'
