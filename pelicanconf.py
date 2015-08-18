4b9b44b95e93b0cc1cf5be86957dc0839a30fb2e
Ellison Leão <ellisonleao@gmail.com>
2015-08-18 19:15:40 -0300

    fix favicon and robots. Resolve #5

--- pelicanconf.py
+++ pelicanconf.py
@@ -51,6 +51,13 @@ MENUITEMS = (
 
 DEFAULT_PAGINATION = 10
 
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/favicon.png']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/favicon.png': {'path': 'favicon.png'}
+}
+
 # ANALYTICS
 GOOGLE_ANALYTICS_UA = 'UA-58961512-1'
 
