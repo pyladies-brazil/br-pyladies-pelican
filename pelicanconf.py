b9a9084463d3eda19abb65fe822c7c1d56e1e6a8
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-09-11 17:02:12 -0300

    Revert "#8 trying to make snap work with heroku"
    This reverts commit 1bf2df4befe2abee80f877bea973896b176a6949.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-
 from __future__ import unicode_literals
 import os
 from datetime import datetime
 from collections import namedtuple
-
-# Bad fix to get common confs
-import sys
-sys.path.append('.')
 
 import yaml
 
