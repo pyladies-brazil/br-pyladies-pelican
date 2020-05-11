98a6719499024ad0a12a49bed3b190acca45a2bd
bianca rosa <me@biancarosa.com.br>
2020-05-11 17:31:20 -0300

    utilizar f-strings
    Co-authored-by: Giovana Morais <giovana.vmorais@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,7 @@ import yaml
 
 AUTHOR = u'Pyladies'
 SITENAME = u'Pyladies Brasil'
-SITEURL = '{}'.format(os.getenv('SITEURL', 'http://localhost:{}'.format(os.getenv('PORT', '8000'))))
+SITEURL = f"{os.getenv('SITEURL', 'http://localhost')}:{os.getenv('PORT', '8000')}"
 TAGLINE = (u'Ninguém pode fazer você se sentir inferior'
            'sem o seu consentimento (Eleanor Roosevelt)')
 DEFAULT_DATE_FORMAT = ('%d-%m-%Y')
