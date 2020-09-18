13e5418150947d650530cf7039da6d00ffa144fc
giovana-morais <giovana.vmorais@gmail.com>
2020-09-18 18:31:32 -0300

    fix: classe da tag html quebrada

--- utils/mapa.py
+++ utils/mapa.py
@@ -25,7 +25,7 @@ def cria_card(capitulo):
     <link rel="stylesheet" href="{}/theme/css/post.css">
     <link rel="stylesheet" href="{}/theme/tipuesearch/tipuesearch.css">""".format(SITEURL, SITEURL, SITEURL, SITEURL, SITEURL, SITEURL)
 
-    img_capitulo = """<div><img class="img-location" src="{}{}" width="100" height="100"></div>""".format(SITEURL, capitulo['image'])
+    img_capitulo = """<div><img class="location-img" src="{}{}" width="100" height="100";></div>""".format(SITEURL, capitulo['image'])
 
     if capitulo.get("url") is not None:
         url_capitulo = """<a target="_blank" href="{}">
@@ -36,69 +36,65 @@ def cria_card(capitulo):
         url_capitulo = """<span class="location-name">Pyladies {} - {}</span>""".format(capitulo['city'], capitulo['state'])
 
 
-    redes_capitulo = """<div class="social-icons>
-    """
-
-##   <div class="social-icons">
-                #     {% if capitulo['twitter'] %}
-                #     <a target="_blank" href="{{ capitulo['twitter'] }}">
-                #       <span class="fa-stack fa-lg">
-                #         <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-                #         <i class="fa fa-twitter fa-stack-1x"></i>
-                #       </span>
-                #     </a>
+    redes_capitulo = """<div class="social-icons">"""
 
     if capitulo.get("twitter") is not None:
-        tt_capitulo = """<a target="_blank" href="{}">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-                    <i class="fa fa-twitter fa-stack-1x"></i>
-                  </span>
-                </a>""".format(capitulo["twitter"])
+        tt_capitulo = """
+                    <a target="_blank" href="{}">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                        <i class="fa fa-twitter fa-stack-1x"></i>
+                      </span>
+                    </a>
+                    """.format(capitulo["twitter"])
         redes_capitulo += tt_capitulo
 
     if capitulo.get("email") is not None:
-        email_capitulo = """<a target="_blank" href="mailto:{}">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-                    <i class="fa fa-envelope fa-stack-1x"></i>
-                  </span>
-                </a>""".format(capitulo["email"])
+        email_capitulo = """
+                        <a target="_blank" href="mailto:{}">
+                          <span class="fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                            <i class="fa fa-envelope fa-stack-1x"></i>
+                          </span>
+                        </a>
+                        """.format(capitulo["email"])
         redes_capitulo += email_capitulo
 
     if capitulo.get("facebook") is not None:
         fb_capitulo = """
-        <a target="_blank" href="{}">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-                    <i class="fa fa-facebook fa-stack-1x"></i>
-                  </span>
-                </a>
-        """.format(capitulo["facebook"])
+                    <a target="_blank" href="{}">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                        <i class="fa fa-facebook fa-stack-1x"></i>
+                      </span>
+                    </a>
+                    """.format(capitulo["facebook"])
         redes_capitulo += fb_capitulo
 
     if capitulo.get("instagram") is not None:
-        ig_capitulo = """<a target="_blank" href="{}">
+        ig_capitulo = """
+                <a target="_blank" href="{}">
                   <span class="fa-stack fa-lg">
                     <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
                     <i class="fa fa-instagram fa-stack-1x"></i>
                   </span>
-                </a>""".format(capitulo["instagram"])
+                </a>
+                """.format(capitulo["instagram"])
         redes_capitulo += ig_capitulo
 
     if capitulo.get("youtube") is not None:
-        yt_capitulo = """<a target="_blank" href="{}">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
-                    <i class="fa fa-youtube-square fa-stack-1x"></i>
-                  </span>
-                </a>""".format(capitulo["youtube"])
+        yt_capitulo = """
+                    <a target="_blank" href="{}">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                        <i class="fa fa-youtube-square fa-stack-1x"></i>
+                      </span>
+                    </a>
+                    """.format(capitulo["youtube"])
         redes_capitulo += yt_capitulo
 
 
     redes_capitulo += "</div>"
-
-    print(redes_capitulo)
 
     card_capitulo += info_css
     card_capitulo += img_capitulo
@@ -139,7 +135,7 @@ def gera_mapa():
         for capitulo in dados_capitulos:
             card_capitulo = cria_card(capitulo)
 
-            iframe = folium.IFrame(html=card_capitulo, width=300, height=300)
+            iframe = folium.IFrame(html=card_capitulo, width=200, height=200)
             popup = folium.Popup(iframe, max_widht=1000)
 
             # print(capitulo['city'], capitulo["lat"], capitulo["lon"])
